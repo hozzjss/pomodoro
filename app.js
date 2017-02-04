16d82d5a4b79f383572662f630a16e131e81e1d1
hozzj <medo3578@gmail.com>
2017-02-04 19:54:18 +0200

    v0.0.1 still needs more work, added some docs in the js file and man f*** css

--- app.js
+++ app.js
@@ -1,36 +1,77 @@
+// DOCS
+//
+// init(option)
+// inits the pomodoro and the break
+//
+// reset()
+// resets everything to it's initial state
+//
+// stop()
+// just stops everything
+//
+// startBreak()
+// starts the break timer
+//
+// addOrSub(option)
+// adds or subtracts minutes to the timer
+//
+// zeroPrefix(num)
+// minute or second is less than 10? prefix it with a 0
+//
+// animateLoader(percentage)
+// takes a percentage and assigns it to the width of the loader
+//
+// countDown()
+// takes the current time and takes one second out of it
+//
+// startCycle()
+// it then updates the ui with the correct remaining time
+// then assigns the loader a width of the percentage of time that is left
+// if it's the break it stops the whole process after it's done
+// if not it starts the break timer after the session is over
+//
+// NTS: too big a fn needs to be broken down into smaller chunks
 $(document).ready(function () {
-  let timeLimit = +($(".timer").text().slice(0,-3)),
-  currentSec,
-  minutes,
-  seconds,
-  timerInterv,
-  firstCycle,
-  isInit;
+  "use strict";
+  //
+  let timeLimit, currentSec, minutes, seconds, timerInterv, breakInit, isInit;
+
+  // effn fns
   const init = (option) => {
     if (isInit) return false;
     if (option == "break") {
       timeLimit = $(".break").length ?
       $("break").text().slice(0, -3) : 5;
+    } else {
+      timeLimit = +($(".timer").text().slice(0,-3));
     }
     currentSec = timeLimit * 60;
-    minutes = timeLimit;
+    minutes = timeLimit - 1;
     seconds = timeLimit * 60;
     isInit = true;
     $(".add, .subtract, .start").addClass("disabled");
     timerInterv = setInterval(startCycle, 1000);
   };
 
+  const reset = () => {
+    isInit = false;
+    breakInit = false;
+    stop();
+    animateLoader(1);
+    $(".add, .subtract, .start").removeClass("disabled");
+    $(".state").text("Session");
+    $(".timer").text(`25:00`);
+  };
+
+  const stop = () => {
+    clearInterval(timerInterv);
+  };
+
   const startBreak = () => {
     reset();
     init("break");
-  };
-
-  const reset = () => {
-    isInit = false, firstCycle = false;
-    $(".add, .subtract, .start").removeClass("disabled");
-    clearInterval(timerInterv);
-    $(".timer").text(`25:00`);
-    animateLoader(1);
+    breakInit = true;
+    $(".state").text("Break");
   };
 
   const addOrSub = option => {
@@ -38,38 +79,35 @@ $(document).ready(function () {
     timeLimit = option == "add" ? timeLimit + 1 : timeLimit - 1;
     $(".timer").text(`${minCalc(timeLimit)}:00`)
   };
-
-
-  const secCalc = sec => {
-    if (sec % 60 == 0) minutes -= 1;
-    return sec % 60 > 9 ? sec % 60 : `0${sec % 60}`;
+  const zeroPrefix = num => {
+    return num < 10 ? `0${num}` : num;
+  };
+  const animateLoader = (percentage) => {
+    percentage = percentage ? percentage : currentSec / seconds;
+    $(".loader").css("width", `${percentage*100}%`);
   };
 
-  const minCalc = min => {
-    if (firstCycle) {
-      firstCycle = false;
-      return minCalc(min - 1);
-    }
-    return min < 10 ? `0${min}` : min;
-  };
-
-  const animateLoader = value => {
-    $(".loader").css("width", `${value*100}%`);
-  };
+  const countDown = () => {
+    currentSec -= 1;
+    if (currentSec % 60 == 0 && minutes !== 0) minutes -= 1;
+  }
 
   const startCycle = () => {
-    firstCycle = true;
-    currentSec -= 1;
-    let value = currentSec / seconds;
-    $(".timer").text(`${minCalc(minutes)}:${secCalc(currentSec)}`);
-    animateLoader(value);
+    countDown();
+    $(".timer")
+    .text(`${zeroPrefix(minutes)}:${zeroPrefix(currentSec % 60)}`);
+    animateLoader();
+    if (breakInit && currentSec <= 0) {
+      stop();
+      return false;
+    }
     if (currentSec <= 0) {
-      clearInterval(timerInterv);
-      setTimeout(startBreak, 3000);
-      isInit = false;
+      stop();
+      startBreak();
     }
   };
 
+  // event handlers
   $(".add").click(function () {
     addOrSub("add");
   });
